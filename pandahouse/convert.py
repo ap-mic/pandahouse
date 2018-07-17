cb7f9c8c276d9c006b2ad8a552587a8f9472e899
SheiUn <d215039@outlook.com>
2018-07-17 23:28:19 +0800

    Resolve KeyError: 'Nullable(String)'

--- pandahouse/convert.py
+++ pandahouse/convert.py
@@ -27,7 +27,7 @@ CH2PD = itemmap(reversed, MAPPING)
 CH2PD['Null'] = 'object'
 CH2PD['Nothing'] = 'object'
 
-NULLABLE_COLS = ['UInt64', 'UInt32', 'UInt16', 'UInt8', 'Float64', 'Float32',
+NULLABLE_COLS = ['String', 'UInt64', 'UInt32', 'UInt16', 'UInt8', 'Float64', 'Float32',
                  'Int64', 'Int32', 'Int16', 'Int8']
 
 for col in NULLABLE_COLS:
